b79e1e510a734286fb6f61162af91863c7726916
Gregor Adams <greg@pixelass.com>
2019-09-12 15:05:53 +0200

    feat(checkbox): use shared colors

--- packages/checkbox/src/theme.ts
+++ packages/checkbox/src/theme.ts
@@ -1,20 +1,4 @@
-const colors = {
-	white: "hsl(0, 0%, 100%)",
-	black: "hsl(0, 0%, 0%)",
-	blue: {
-		600: "hsl(215, 75%, 51%)",
-		500: "hsl(215, 85%, 55%)",
-		200: "hsl(214, 88%, 69%)",
-		100: "hsl(214, 82%, 75%)"
-	},
-	grey: {
-		600: "hsl(0, 0%, 34%)",
-		300: "hsl(0, 0%, 43%)",
-		200: "hsl(0, 0%, 51%)",
-		100: "hsl(60, 1%, 79%)",
-		50: "hsl(60, 3%, 88%)"
-	}
-};
+import {colors} from "@react-osx/colors";
 
 const decoration = {
 	shadow: {
@@ -33,10 +17,10 @@ export const theme = {
 		},
 		default: {
 			border: {
-				color: colors.grey[200]
+				color: colors.grey[500]
 			},
 			borderRadius: "3px",
-			boxShadow: `inset 0 0.5px 0.5px 0 ${colors.grey[200]}, ${decoration.shadow[1]}`,
+			boxShadow: `inset 0 0.5px 0.5px 0 ${colors.grey[500]}, ${decoration.shadow[1]}`,
 			font: {
 				size: "12px",
 				weight: "normal",
@@ -44,18 +28,18 @@ export const theme = {
 			},
 			color: colors.white,
 			background: {
-				image: `linear-gradient(0deg, ${colors.grey[600]}, ${colors.grey[300]})`,
-				color: colors.grey[600]
+				image: `linear-gradient(0deg, ${colors.grey[650]}, ${colors.grey[550]})`,
+				color: colors.grey[650]
 			},
 			active: {
 				border: {
-					color: colors.blue[200]
+					color: colors.blue[300]
 				},
-				boxShadow: `inset 0 0.5px 0.5px 0 ${colors.blue[200]}, ${decoration.shadow[1]}`,
+				boxShadow: `inset 0 0.5px 0.5px 0 ${colors.blue[300]}, ${decoration.shadow[1]}`,
 				color: colors.white,
 				background: {
-					image: `linear-gradient(0deg, ${colors.blue[600]}, ${colors.blue[500]})`,
-					color: colors.blue[600]
+					image: `linear-gradient(0deg, ${colors.blue[500]}, ${colors.blue[450]})`,
+					color: colors.blue[500]
 				}
 			}
 		}
